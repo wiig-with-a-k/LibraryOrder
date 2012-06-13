127bda747a48f953a3a30d6cb29061d4fa341249
Erlord <erlend.wiig@gmail.com>
2012-06-13 18:17:34 +0200

    Added albums and songs to respective views.

--- libraryorder.js
+++ libraryorder.js
@@ -6,63 +6,17 @@ var library = models.library;
 exports.init = init;
 
 function init() {
-	console.log('Booting');
+	console.log('init');
 	
-	var header = document.getElementById("header");
-	
-	fillArtistView();
-	/*
-	for(var i=0; i<library.albums.length; i++) {
-		console.log(library.albums[i].name);
-	
-		header.innerHTML += library.albums[i].name + '\n';
-	}
-
-	
-
-	player.observe(models.EVENT.CHANGE, function (e) {
-		// Only update the page if the track changed
-		if (e.data.curtrack == true) {
-			updatePageWithTrackDetails();
-		}
-	});
-	*/
+	fillViewWithData($('#ArtistsList'), library.artists);
+	fillViewWithData($('#AlbumsList'), library.albums);
+	fillViewWithData($('#SongsList'), library.tracks);
 }
 
-function fillArtistView() {
-	var artistView;
-	
-	artistView = document.getElementById('ArtistView');
-	
-	$('#ArtistView').append("<ul id ='artistList'></ul>");
-	
-	
-	
-	for(var i=0; i<library.artists.length; i++) {
-		console.log(library.artists[i].name);
+function fillViewWithData (view, data) {
+  for(var i=0; i<data.length; i++) {
+		console.log(data[i].name);
 		
-		$('#artistList').append('<li>' + library.artists[i].name + '</li>');
-		
-	}
-	/*
-	    $('#myDiv').append("<ul id='newList'></ul>");
-    for (cnt = 0; cnt < someList.length; cnt++) {
-          $("#newList").append("<li>"+someList[cnt].FirstName + ":" + someList[cnt].LastName+"</li>");
-    }
-	*/
-}
-
-function updatePageWithTrackDetails() {
-	
-	var header = document.getElementById("header");
-
-	// This will be null if nothing is playing.
-	var playerTrackInfo = player.track;
-
-	if (playerTrackInfo == null) {
-		header.innerText = "Nothing playing!";
-	} else {
-		var track = playerTrackInfo.data;
-		header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
+		view.append('<li>' + data[i].name + '</li>');
 	}
 }
